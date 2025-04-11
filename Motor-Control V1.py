62a7f28830364cdc421aa2df8dbe27817d0c5d93
newtons-lastlaw <jadonnewton23@icloud.com>
2025-04-11 01:25:12 -0500

    testing

--- Motor-Control V1.py
+++ Motor-Control V1.py
@@ -5,24 +5,23 @@ SPEED = 9
 
 class BrailleWheel:
     def __init__(self, motor_pins: tuple):
-        # Initialize the Motor object directly
         self.motor = Motor(forward=motor_pins[0], backward=motor_pins[1])
 
     def next_letter(self):
-        self.motor.forward()  # Move forward
-        sleep(SPEED)          # Wait for the desired duration
-        self.motor.stop()     # Stop the motor
+        self.motor.forward()  
+        sleep(SPEED)          
+        self.motor.stop()  
 
     def previous_letter(self):
-        self.motor.backward() # Move backward
-        sleep(SPEED)          # Wait for the desired duration
-        self.motor.stop()     # Stop the motor
+        self.motor.backward()
+        sleep(SPEED)          
+        self.motor.stop()     
 
 ########################main######################
 
 # Create a BrailleWheel instance with motor pins (4, 24)
-m1 = BrailleWheel((4, 24))
-m2 = BrailleWheel((17,18))
+m1 = BrailleWheel((16, 17))
+m2 = BrailleWheel((18,19))
 m1.next_letter()
 m2.next_letter()
 m1.previous_letter()
