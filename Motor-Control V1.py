91faa983f75f614c9ae9e8df5279d423e65922fe
newtons-lastlaw <jadonnewton23@icloud.com>
2025-04-12 16:39:08 -0500

    made it an instance variable instead

--- Motor-Control V1.py
+++ Motor-Control V1.py
@@ -6,6 +6,7 @@ SPEED = 9
 class BrailleWheel:
     def __init__(self, motor_pins: tuple):
         self.motor = Motor(forward=motor_pins[0], backward=motor_pins[1])
+        self.value = 1
 
     def next_letter(self):
         self.motor.forward()  
@@ -22,8 +23,6 @@ class BrailleWheel:
 # Create a BrailleWheel instance with motor pins (4, 24)
 m1 = BrailleWheel((16, 17))
 m2 = BrailleWheel((18,19))
-m1.value = 1
-m2.value =1
 m1.next_letter()
 m2.next_letter()
 m1.previous_letter()
