75d2810d1de4813321178667ba26308484ef4315
Hayden Mersky <haydenmersky005@gmail.com>
2025-04-13 16:55:12 -0500

    Got rid of motor functionality and added parts of servo initialization for the switch to servos

---
+++ servo_function/servo_function.ino
@@ -0,0 +1,34 @@
+#include<Servo.h>
+
+Servo servo1; // Creat servo object which we'll put on GPIO 2
+Servo servo2; // Creat servo object which we'll put on GPIO 3
+Servo servo3; // Creat servo object which we'll put on GPIO 4
+Servo servo4; // Creat servo object which we'll put on GPIO 5
+Servo servo5; // Creat servo object which we'll put on GPIO 6
+Servo servo6; // Creat servo object which we'll put on GPIO 7
+Servo servo7; // Creat servo object which we'll put on GPIO 8
+Servo servo8; // Creat servo object which we'll put on GPIO 9
+Servo servo9; // Creat servo object which we'll put on GPIO 10
+Servo servo10; // Creat servo object which we'll put on GPIO 11
+Servo servo11; // Creat servo object which we'll put on GPIO 12
+
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(2);
+  servo2.attach(3);
+  servo3.attach(4);
+  servo4.attach(5);
+  servo5.attach(6);
+  servo6.attach(7);
+  servo7.attach(8);
+  servo8.attach(9);
+  servo9.attach(10);
+  servo10.attach(11);
+  servo11.attach(12);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
