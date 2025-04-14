a93d4f9c3298458bf2f3e2b6ecf75b9041321fe3
newtons-lastlaw <jadonnewton23@icloud.com>
2025-04-14 00:05:48 -0500

    Updated the Servos code to work with a given character

--- servo_function/servo_function.ino
+++ servo_function/servo_function.ino
@@ -1,5 +1,6 @@
 #include<Servo.h>
 
+
 Servo servo1; // Creat servo object which we'll put on GPIO 2
 Servo servo2; // Creat servo object which we'll put on GPIO 3
 Servo servo3; // Creat servo object which we'll put on GPIO 4
@@ -12,8 +13,21 @@ Servo servo9; // Creat servo object which we'll put on GPIO 10
 Servo servo10; // Creat servo object which we'll put on GPIO 11
 Servo servo11; // Creat servo object which we'll put on GPIO 12
 
+const float degree = 12.8571;  // each character is 12.8571 degrees apart
+float servoPositions[28]; // Array to hold the servo positions for each character
+char characters [28] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','space','prefix'}; // Array to hold the characters corresponding to the servo positions
+
 void setup() {
   // put your setup code here, to run once:
+  for (int i = 0; i < 2;: i++) {
+    servoPositions[i] = (i+1) * degree;  // sets the degree for all the characters
+  }
+
+  servoPositions[26] = 27 * degree;  // degree for the space character
+  servoPositions[27] = 28 * degree;  // degree for the prefix character
+
+// Attach the servos to the pins 
   servo1.attach(2);
   servo2.attach(3);
   servo3.attach(4);
@@ -26,6 +40,70 @@ void setup() {
   servo10.attach(11);
   servo11.attach(12);
 
+  int getIndex(char c){
+    c = tolower(c)
+    for (int i = 0; i < 28; i++){
+      if (characters[i] == c) return i:  // returns the index of the character in the array of the given character
+    }
+    return -1
+  }
+
+  // UDF for each servo to move to the position of the character
+  void moveServo1(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo1.write(servoPositions[index]);
+  }
+  
+  void moveServo2(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo2.write(servoPositions[index]);
+  }
+  
+  void moveServo3(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo3.write(servoPositions[index]);
+  }
+  
+  void moveServo4(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo4.write(servoPositions[index]);
+  }
+  
+  void moveServo5(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo5.write(servoPositions[index]);
+  }
+  
+  void moveServo6(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo6.write(servoPositions[index]);
+  }
+  
+  void moveServo7(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo7.write(servoPositions[index]);
+  }
+  
+  void moveServo8(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo8.write(servoPositions[index]);
+  }
+  
+  void moveServo9(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo9.write(servoPositions[index]);
+  }
+  
+  void moveServo10(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo10.write(servoPositions[index]);
+  }
+  
+  void moveServo11(char c) {
+    int index = getIndex(c);
+    if (index != -1) servo11.write(servoPositions[index]);
+
+}
 }
 
 void loop() {
